430eedd96d2f480583c380166a4ba9ace8359311
amir <mirzayi994@gmail.com>
2025-05-15 23:14:22 +0330

    initial compose files

---
+++ sftp/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for user_home in /home/* ; do
+  if [ -d "$user_home" ]; then
+    username=`basename $user_home`
+    echo "Setup $user_home/share folder for $username"
+    mkdir -p $user_home/share/in
+    mkdir -p $user_home/share/out
+    chown -R $username:users $user_home/share
+  fi
+done
